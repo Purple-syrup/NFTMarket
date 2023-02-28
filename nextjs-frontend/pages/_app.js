22a6d701456975dce945ca377405c2284870a053
Purple-syrup <godhanded0@gmail.com>
2023-02-28 20:55:38 +0000

    set up

--- nextjs-frontend/pages/_app.js
+++ nextjs-frontend/pages/_app.js
@@ -1,43 +1,39 @@
-import "@/styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import Head from "next/head";
+import "@/styles/globals.css"
+import "@rainbow-me/rainbowkit/styles.css"
+import Head from "next/head"
 
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-  darkTheme,
-} from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-import Header from "../components/Header";
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit"
+import { configureChains, createClient, WagmiConfig } from "wagmi"
+import { goerli } from "wagmi/chains"
+import { publicProvider } from "wagmi/providers/public"
+import Header from "../components/Header"
 
-const { chains, provider } = configureChains([goerli], [publicProvider()]);
+const { chains, provider } = configureChains([goerli], [publicProvider()])
 const { connectors } = getDefaultWallets({
-  appName: "NFT Marketplace",
-  chains,
-});
+    appName: "NFT Marketplace",
+    chains,
+})
 const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
+    autoConnect: true,
+    connectors,
+    provider,
+})
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="NFT Marketplace" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains} theme={darkTheme()}>
-          <Header />
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <WagmiConfig client={wagmiClient}>
+                <RainbowKitProvider chains={chains} theme={darkTheme()}>
+                    <Header />
+                    <Component {...pageProps} />
+                </RainbowKitProvider>
+            </WagmiConfig>
+        </>
+    )
 }
