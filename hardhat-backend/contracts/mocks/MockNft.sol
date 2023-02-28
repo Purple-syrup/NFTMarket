e0778e784fc2d993bb1009999be9b1c6f6f4d9d8
Godand <aezeigwe2003@gmail.com>
2023-02-28 20:10:05 +0000

    next fe

--- hardhat-backend/contracts/mocks/MockNft.sol
+++ hardhat-backend/contracts/mocks/MockNft.sol
@@ -5,16 +5,25 @@ pragma solidity 0.8.12;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockNft is ERC721 {
-    uint256 private tokenCounter;
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 private s_tokenCounter;
 
-    constructor() ERC721("MockNft", "MNT") {}
-
-    function mint() external {
-        _safeMint(msg.sender, tokenCounter);
-        tokenCounter += 1;
+    constructor() ERC721("MockNft", "MNT") {
+        s_tokenCounter = 0;
     }
 
-    function getTokenCounter() external view returns (uint256) {
-        return tokenCounter;
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        // require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
--- hardhat-backend/deploy/99-update-frontend.js
+++ hardhat-backend/deploy/99-update-frontend.js
@@ -1,13 +1,25 @@
 const { network, ethers } = require("hardhat")
 const fs = require("fs")
 
-const frontendContractFile = "../../nextjs-frontend/constants/contractAddresses.json"
+
+const frontendContractFile ="../../nextjs-frontend/constants/contractAddresses.json";
+const frontendContractAbi="../../nextjs-frontend/constants/"
 
 module.exports = async () => {
     if (process.env.UPDATE_FRONTEND) {
         console.log("updating frontend ......")
         await updateContractAddresses()
+      await updateAbi()
     }
+}
+
+async function updateAbi(){
+  const nftMarketplace = await ethers.getContract("NftMarketplace")
+  fs.writeFileSync(`${frontendContractAbi}NftMarketplace.json`,nftMarketplace.interface.format(ethers.utils.FormatTypes.json))
+
+  const mockNft = await ethers.getContract("MockNft")
+    fs.writeFileSync(`${frontendContractAbi}MockNft.json`,mockNft.interface.format(ethers.utils.FormatTypes.json))
+  
 }
 
 async function updateContractAddresses() {
